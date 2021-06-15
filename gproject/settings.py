da998743aec0672419d1d3cee77765e2bac1dca5
xephtar <manger2105@hotmail.com>
2021-06-15 21:09:41 +0300

    deploy

--- gproject/settings.py
+++ gproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'n!)#t^92oq9f06%fzwl68qdnm*ran39i17m^vu5hm^@(oy^eg4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', False)
 
 ALLOWED_HOSTS = []
 
