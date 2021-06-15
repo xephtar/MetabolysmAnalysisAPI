184a952a932f4a3efc4c21226d18d583319f39e4
xephtar <manger2105@hotmail.com>
2021-06-15 20:44:19 +0300

    make it better

--- gproject/settings.py
+++ gproject/settings.py
@@ -192,7 +192,7 @@ LOGGING = {
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/",
+        "LOCATION": os.environ.get('REDIS_URL'),
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient"
         },
